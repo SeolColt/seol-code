803be8462a61f3191d6b17d6d693b30371fc9f57
Seol_Colt <florentinostewart@gmail.com>
2025-11-02 23:58:47 +0800

    new detail

--- 11.2 drill.c
+++ 11.2 drill.c
@@ -2,7 +2,7 @@
 int main(){
 
 
-//print12345
+//print12345                                              //1.      program to print numbers from 1 to 5    
     // int a = 1;
     // for (a=1;a<=5;a++){
     //     printf("%d",a);
@@ -10,22 +10,37 @@ int main(){
     // return 0;
 
 
-//print12345 in reserve order
+//print12345 in reserve order                              //2.   program to print numbers from 5 to 1
     //  int a ;
     // for (a=5;a>=1;a--){
     //     printf("%d",a);
     // }
 
 //print the sum of the double number from 1 to 100
-    int count;
-    int sum = 0;  //to store the sum
-    for(count=1;count <= 100;count++){
-        if(count %2 ==0){
-            printf("%d\n",sum += count);
+//    int count;
+//     int sum = 0;  //to store the sum
+//     for(count=1;count <= 100;count++){
+//         if(count %2 ==0){
+//             printf("%d\n",sum += count);
+//         }
+
+
+
+
+
+                                                          //3. print the number which can be divided by 24 between a and b
+    int number;                                           //the very number between a and b
+    printf("enter the min and max number:");              //promp the user to enter the min and max number
+    int a,b;                                              //the min and max number
+    int total = 0;                                         //the total of the very number between a and b
+    scanf("%d %d",&a,&b);                                  //get the min and max number from the user
+    for(number = a; number<=b;number++){    
+        if(number % 24 == 0){
+            total++;                                       //increase the total when the number can be divided by 24
+            printf("%d is divisible by 24 \n",number);    //indicate the number which can be divided by 24
         }
-
     }
-
+     printf("the total is %d\n",total);                     //print the total of the very number between a and b    
 
 
     return 0;
