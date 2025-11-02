d961397ccffcada170a37a4952d02c6749b947a0
Seol_Colt <florentinostewart@gmail.com>
2025-11-02 19:39:59 +0800

    first commit

---
+++ 11.2 drill.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+int main(){
+    // int a = 1;
+    // for (a=1;a<=5;a++){
+    //     printf("%d",a);
+    // }
+    // return 0;
+
+     int a ;
+    for (a=5;a>=1;a--){
+        printf("%d",a);
+    }
+    return 0;
+}
+
+
