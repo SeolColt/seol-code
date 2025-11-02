bee9eeebc83488fcdb2731e2aac660e35d8b7d4b
Seol_Colt <florentinostewart@gmail.com>
2025-11-02 20:39:15 +0800

    more detail

--- 11.2 drill.c
+++ 11.2 drill.c
@@ -1,15 +1,33 @@
 #include "stdio.h"
 int main(){
+
+
+//print12345
     // int a = 1;
     // for (a=1;a<=5;a++){
     //     printf("%d",a);
     // }
     // return 0;
 
-     int a ;
-    for (a=5;a>=1;a--){
-        printf("%d",a);
+
+//print12345 in reserve order
+    //  int a ;
+    // for (a=5;a>=1;a--){
+    //     printf("%d",a);
+    // }
+
+//print the sum of the double number from 1 to 100
+    int count;
+    int sum = 0;  //to store the sum
+    for(count=1;count <= 100;count++){
+        if(count %2 ==0){
+            printf("%d\n",sum += count);
+        }
+
     }
+
+
+
     return 0;
 }
 
