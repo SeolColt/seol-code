f3a858180d265b37f0a5e73a2bbc036aaabf841a
Seol_Colt <florentinostewart@gmail.com>
2025-11-03 22:54:11 +0800

    1

--- 11.3 while drill.c
+++ 11.3 while drill.c
@@ -1,9 +1,9 @@
 #include "stdio.h"
 int main(){
     int a = 1,i;
-    printf("Enter your name: ");  
-    char name[50];
-    scanf("%s", name);
+    printf("Enter your name: ");        //input name
+    char name[50];                    
+    scanf("%s", name);                  //read name
     printf("Enter the number of times to print your name: ");    
     scanf("%d", &i);
     while(a<= i ){
