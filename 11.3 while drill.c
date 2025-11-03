23e685b8ae58cfb014ed8dc83c51f31b2fdec5b8
Seol_Colt <florentinostewart@gmail.com>
2025-11-03 21:59:36 +0800

    whi

---
+++ 11.3 while drill.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+int main(){
+    int a = 1,i;
+    printf("Enter your name: ");  
+    char name[50];
+    scanf("%s", name);
+    printf("Enter the number of times to print your name: ");    
+    scanf("%d", &i);
+    while(a<= i ){
+        printf("Hello,%s\n", name);
+        a++;
+
+    }
+return 0;
+
+
+}
