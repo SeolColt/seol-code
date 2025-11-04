397e438acd64cfb435b2279ddecb7bd5cb07e59a
Seol_Colt <florentinostewart@gmail.com>
2025-11-05 00:46:04 +0800

    new

---
+++ 11.4--11.5 algorithm.c
@@ -0,0 +1,36 @@
+#include "stdio.h"
+int main(){
+//part 1 :      judge whether a decimal is the power of 2
+    // int n;                                          //the number to be judged
+    // int count =0;                                   //the exponent if n is the power of 2
+    // printf("Enter a positive integer: ");           //input n
+    // scanf("%d", &n);                                //read n
+    // while(n>=1 && n%2==0){                          //keep dividing n by 2 while n is even and greater than or equal to 1
+    // n /= 2;                                         //conduct "divide n by 2"
+    // count += 1;                                     //increase the exponent by 1
+    // }
+    // if(n==1){                                                   //if n is reduced to 1, it means the oringinal n is a power of 2
+    //     printf("n is a power of 2\n");                          //output result
+    //     printf("n can be illustrated with 2^%d\n", count);      //output the exponent
+    // }
+    // else{                                                       //if n is not reduced to 1, it means the original n is not a power of 2
+    //     printf("n is not a power of 2\n");
+    // }
+//part 2 :      calculate the times to fold a paper to exceed the height of the Everest                    
+    // double thickness = 0.1;                                                //initial thickness of the paper in mm
+    // double everest_height = 8848000;                                       //height of the Everest in mm
+    // int fold_count = 0;                                                    //the times to fold the paper
+    // while(thickness <= everest_height){                                    //keep folding the paper while its thickness is less than or equal to the height of the Everest
+    //     thickness *=2;                                                     //double the thickness of the paper
+    //     fold_count +=1;                                                     //increase the fold count by 1
+    // }
+    //     printf("the times to fold the paper to exceed the height of the Everest is: %d\n", fold_count);         //output the fold count
+
+//part3
+
+
+
+
+
+return 0;
+}
