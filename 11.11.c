03710b3a08bc67523742e723773bd1651684b7c1
Seol_Colt <florentinostewart@gmail.com>
2025-11-11 15:30:36 +0800

    new

---
+++ 11.11.c
@@ -0,0 +1,54 @@
+#include "stdio.h"
+int main(){
+
+//part 1 倒三角
+    // int i =1;
+    // int j =1;
+    // while(i<=7){
+    //     for(j=i;j<=7;j++){
+    // printf("*");
+    //     }
+    // printf("\n");
+    // i++;
+    // }
+
+//part 2 正三角
+    // for(int a = 1;a<=5;a++){
+    //     for(int b = 1;b<=a;b++){
+    //     printf("*");
+    //     }
+    //     printf("\n");
+    // }
+
+//prat 3 乘法表
+    // int j = 1;
+    // for(int a = 1;a<=9;a++){
+    //     for(j=1;j<=a;j++){
+    //         printf("%d * %d = %d\t",j ,a,j*a);
+    //     }
+    //     printf("\n");
+    // }
+
+//part 4 sum of a number's each digit that equal 15 (number ranges from 0 to 1000)
+ long number, original;
+    printf("enter a number between 0 and 1000: ");
+    scanf("%ld", &number);
+    original = number;  // 保存原始输入值
+
+    int sum = 0;
+    int digit;
+
+    while (number != 0) {
+        digit = number % 10;
+        sum = sum + digit;
+        number = number / 10;
+    }
+
+    if (sum == 15) {
+        printf("the sum of %ld's each digit equal 15\n", original);
+    } else {
+        printf("the sum of %ld's each digit does not equal 15\n", original);
+    }
+
+return 0;
+}
