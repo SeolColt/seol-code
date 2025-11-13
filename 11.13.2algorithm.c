4a3084e364d1425869ac69cb7fee896a33c96e1d
Seol_Colt <florentinostewart@gmail.com>
2025-11-13 21:00:30 +0800

    feat

--- 11.13.2algorithm.c
+++ 11.13.2algorithm.c
@@ -2,21 +2,33 @@
 #include <time.h>
 #include <stdlib.h> 
 
-
+int correct(int array[], int len,int num){
+    for(int i = 0;i<len;i++){
+        if(array[i]==num){
+            return 1;}
+         }
+     return 0;
+}
 
 
 int main(){
     srand(time(NULL));                                                          //set seed for random number generation
     int array[10]={};
-
-    double sum = 0 ;
     int len = sizeof(array)/sizeof(int);
     printf("generated numbers :\n");
-    for(int i = 0;i<len;i++){
+    for(int i = 0;i<len;){
         int rand1 = rand()%100+1;
+        int temp = correct(array,len,rand1);
+        if(temp==0){
         array[i] = rand1;
-        printf("%d \n", array[i]);
+        printf("%d\n", array[i]);
+        i++;
+        continue;
+        }
+   
+        printf("the number repeated:%d\n",rand1);
     }
+        double sum = 0 ;
     for(int i=0;i<len;i++){
     sum += array[i];
     }   
